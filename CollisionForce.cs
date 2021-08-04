7e4d9820cd3006a2ac5d2590a8b2efe7d957d62e
AbnnerU <69600765+AbnnerU@users.noreply.github.com>
2021-08-04 17:29:12 -0300

    Add files via upload

---
+++ CollisionForce.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CollisionForce : MonoBehaviour
+{
+    [SerializeField] private int layerIDToDetection;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == layerIDToDetection)
+        {
+            Vector3 collisionForce = collision.impulse / Time.fixedDeltaTime;
+            print(collisionForce);
+        }
+    }
+}
---
+++ MovementLimits.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementLimits : MonoBehaviour
+{
+    [SerializeField] private Transform limitXReference;
+
+    [SerializeField] private Transform limitZReference;
+
+    private float limitX;
+    private float limitZ;
+
+    private void Awake()
+    {
+        limitX = limitXReference.position.x;
+
+        limitZ = limitZReference.position.z;
+
+    }
+
+    public void CalculateObjectMovement(Vector3 movementVector)
+    {
+
+        Vector3 nextPosition = transform.position + movementVector;
+
+        print(nextPosition);
+
+        float movementX = Mathf.Clamp(nextPosition.x, -limitX, limitX);
+
+        print("X :"+movementX);
+
+        float movementZ = Mathf.Clamp(nextPosition.z, -limitZ, limitZ);
+
+        print("Z ;"+movementZ);
+
+        Vector3 filtredPosition = new Vector3(movementX, nextPosition.y, movementZ);
+
+        transform.position = filtredPosition;
+
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (limitZReference != null)
+        {
+            Gizmos.color = Color.blue;
+
+            Gizmos.DrawSphere(new Vector3(-limitXReference.position.x, limitXReference.position.y, limitXReference.position.z), 1f);
+
+
+            Gizmos.color = Color.red;
+            
+            Gizmos.DrawSphere(new Vector3(limitZReference.position.x, limitZReference.position.y, -limitZReference.position.z), 1f);
+        }
+    }
+}
